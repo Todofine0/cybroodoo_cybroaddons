4189a05dafd01689684f1e5c5128415eb3628ff4
Raymond Salomon <raymondsalomonsoto@gmail.com>
2024-04-30 17:00:25 -0400

    Modificado el Martes 30 Abril 2024 a las 5:00 pm

--- advanced_loan_management/models/loan_request.py
+++ advanced_loan_management/models/loan_request.py
@@ -255,12 +255,14 @@ class LoanRequest(models.Model):
                     'amount': amount,
                     'interest_amount': interest_amount,
                     'total_amount': total_amount,
-                    'interest_account_id': self.env.ref('advanced_loan_management.'
-                                                        'loan_management_'
-                                                        'inrst_accounts').id,
-                    'repayment_account_id': self.env.ref('advanced_loan_management.'
-                                                         'demo_'
-                                                         'loan_accounts').id,
+                    'interest_account_id': self.interest_account_id.id,
+                    # 'interest_account_id': self.env.ref('advanced_loan_management.'
+                    #                                     'loan_management_'
+                    #                                     'inrst_accounts').id,
+                    'repayment_account_id': self.repayment_account_id.id,
+                    # 'repayment_account_id': self.env.ref('advanced_loan_management.'
+                    #                                      'demo_'
+                    #                                      'loan_accounts').id,
                     'loan_id': loan.id})
                 date_start += relativedelta(months=1)
         return True
