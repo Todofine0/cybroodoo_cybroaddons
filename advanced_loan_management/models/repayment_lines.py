efed4d8df547af08815303b3c83d8d448dad7ce1
Raymond Salomon <raymondsalomonsoto@gmail.com>
2024-05-01 20:41:40 -0400

    Modificado el Lunes 29 Abril 2024 a las 8:41 pm

--- advanced_loan_management/models/repayment_lines.py
+++ advanced_loan_management/models/repayment_lines.py
@@ -64,11 +64,11 @@ class RepaymentLine(models.Model):
                                       help="Journal Record")
     interest_account_id = fields.Many2one('account.account',
                                           string="Interest",
-                                          default=lambda self: self.env['res.config.settings'].sudo().get_values().get('interest_account_id'),
+                                          default=self.env['res.config.settings'].sudo().get_values().get('interest_account_id'),
                                           help="Account For Interest")
     repayment_account_id = fields.Many2one('account.account',
                                           string="Repayment",
-                                          default=lambda self: self.
+                                          default=self.
                                           env['account.account'].
                                           search([('code', 'ilike', '200012')]),
                                           help="Account For Repayment")
