19581928edb59048ab709fff06e2afd29a490d88
Raymond Salomon <raymondsalomonsoto@gmail.com>
2024-05-04 06:03:14 -0400

    Modificado el Sábado 4 Mayo 2024 a las 6:03 am

--- advanced_loan_management/models/loan_request.py
+++ advanced_loan_management/models/loan_request.py
@@ -91,13 +91,13 @@ class LoanRequest(models.Model):
                                        string="Debit account", 
                                        default=lambda self: self.
                                       env['account.account'].
-                                      search([('code', 'ilike', '200019')]),
+                                      search([('code', 'ilike', '200012')]),
                                        help="Choose account for disbursement debit")
     credit_account_id = fields.Many2one('account.account', 
                                        string="Credit account", 
                                        default=lambda self: self.
                                       env['account.account'].
-                                      search([('code', 'ilike', '200019')]),
+                                      search([('code', 'ilike', '1111001')]),
                                        help="Choose account for disbursement credit")
     reject_reason = fields.Text(string="Reason", help="Displays "
                                                       "rejected reason")
